4e17b5359d7a2901233cbc6a3637ec430a1c0374
Cristian Rubio <44789319+cristianrubioa@users.noreply.github.com>
2021-03-18 01:44:59 -0500

    KITTI format pose

--- src/odomEstimationNode.cpp
+++ src/odomEstimationNode.cpp
@@ -121,6 +121,58 @@ void odom_estimation(){
             laserOdometry.pose.pose.position.y = t_current.y();
             laserOdometry.pose.pose.position.z = t_current.z();
             pubLaserOdometry.publish(laserOdometry);
+            
+            ///////////////////// KITTI format pose ///////////////////
+
+            Eigen::Matrix3d R = q_current.toRotationMatrix();
+
+            if (init_flag==true)
+            {
+
+            H_init<< R.row(0)[0],R.row(0)[1],R.row(0)[2],t_current.x(),
+                     R.row(1)[0],R.row(1)[1],R.row(1)[2],t_current.y(),
+                     R.row(2)[0],R.row(2)[1],R.row(2)[2],t_current.z(),
+                     0,0,0,1;
+
+            init_flag=false;
+
+            }
+
+            H_rot<<	0,-1,0,0, 
+                    0,0,-1,0,
+                    1,0,0,0,
+                    0,0,0,1;
+
+            H<<  R.row(0)[0],R.row(0)[1],R.row(0)[2],t_current.x(),
+                 R.row(1)[0],R.row(1)[1],R.row(1)[2],t_current.y(),
+                 R.row(2)[0],R.row(2)[1],R.row(2)[2],t_current.z(),
+                 0,0,0,1;
+
+            H = H_rot*H_init.inverse()*H; 
+
+            std::ofstream foutC(RESULT_PATH, std::ios::app);
+
+            foutC.setf(std::ios::scientific, std::ios::floatfield);
+            foutC.precision(6);
+
+            for (int i = 0; i < 3; ++i)
+            {
+                    for (int j = 0; j < 4; ++j)
+                    {
+                            if(i==2 && j==3)
+                            {
+                                    foutC <<H.row(i)[j]<< std::endl ;
+                            }
+                            else
+                            {
+                                    foutC <<H.row(i)[j]<< " " ;
+                            }
+                    }
+            }
+
+            foutC.close();
+
+        //////////////////////////////////////////////////
 
         }
         //sleep 2 ms every time
